6443d4b5d07d50fb015570373e52e7dcb9159295
pawrut <74461506+pawrut@users.noreply.github.com>
2020-12-12 14:58:21 +0100

    Update HelloWorld.java

--- src/pl/edu/wszib/HelloWorld.java
+++ src/pl/edu/wszib/HelloWorld.java
@@ -3,5 +3,6 @@ package pl.edu.wszib;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+         System.out.println("Hello Pawel ! ");
     }
 }
